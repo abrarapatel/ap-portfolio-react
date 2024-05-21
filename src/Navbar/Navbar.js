90a9452b11479c5baa374074540da29d45e43eff
abrarapatel <abrar44120@gmail.com>
2024-05-21 09:14:19 -0400

    First Commit

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+            <h1>Navbar</h1>
+        </div>
+    );
+}
+
+export default Navbar;
